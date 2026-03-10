23898f91f1f0aa011c3c6e9f853b03625548fbe1
horsee77 <lucashorsee@icloud.com>
2026-03-10 15:36:23 -0300

    Integrate Facebook Pixel tracking in layout
    Added Facebook Pixel tracking script to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { GeistSans } from "geist/font/sans";
 import { getCart } from "lib/shopify";
 import { ReactNode } from "react";
 import { Toaster } from "sonner";
+import Script from "next/script";
 import "./globals.css";
 import { baseUrl } from "lib/utils";
 
@@ -35,6 +36,38 @@ export default async function RootLayout({
   return (
     <html lang="pt-BR" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+        <Script id="meta-pixel-base" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '2047714559511333');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2047714559511333&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <CartProvider cartPromise={cart}>
           <Navbar />
           <main>
